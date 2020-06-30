c53ad5be005e34a1fd88b09e1e68f83257ab441c
駒田　亮太 <ry.0901.yu@gmail.com>
2020-06-30 09:09:47 +0900

    ユーザ認証機能を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,12 +22,13 @@ class Article < ApplicationRecord
   validate :validate_title_and_content_length
 
   def display_created_at
-    I18n.l(self.created_at, format: :default)
+    I18n.l(created_at, format: :default)
   end
 
   private
+
   def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
+    char_count = title.length + content.length
     errors.add(:content, '100文字以上で!!') unless char_count > 100
   end
 end
